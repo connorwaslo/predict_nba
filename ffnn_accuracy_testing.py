bca1b02dd4a1e288bc6045edfe7e99f860097ff9
connorwaslo <connorwaslo@gmail.com>
2019-05-15 16:52:44 -0700

    Tested varying data sets
    Tried all different datasets and network layouts. Inconclusive, gonna have to go through this with k-folds and really compare. Eye-test didn't prove much unfortunately.

--- ffnn_accuracy_testing.py
+++ ffnn_accuracy_testing.py
@@ -1,13 +1,36 @@
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
 def features(file):
-    use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'H_FG', 'H_3P', 'H_FT']
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'H_FG', 'H_3P', 'H_FT']
+    # use_cols = ['GAME_ID', 'A_FG', 'A_FGA', 'A_3P', 'A_3PA', 'A_FT', 'A_FTA', 'A_ORB', 'A_DRB', 'A_TRB', 'A_AST', 'A_STL', 'A_BLK', 'A_TOB', 'A_PF', 'A_AVG_PTS',
+    #             'H_FG', 'H_FGA', 'H_3P', 'H_3PA', 'H_FT', 'H_FTA', 'H_ORB', 'H_DRB', 'H_TRB', 'H_AST', 'H_STL', 'H_BLK', 'H_TOB', 'H_PF', 'H_AVG_PTS']
+
+    # 256: val_loss = 138.7081, 60.46%
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'A_ORB', 'A_DRB', 'A_AST', 'A_STL', 'A_BLK', 'A_TOB', 'A_PF',
+    #             'H_FG', 'H_3P', 'H_FT', 'H_ORB', 'H_DRB', 'H_AST', 'H_STL', 'H_BLK', 'H_TOB', 'H_PF']
+
+    # 128: val_loss = 129.4028, 64.45%
+    use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'A_ORB', 'A_DRB', 'A_AST', 'A_STL', 'A_BLK',
+                'H_FG', 'H_3P', 'H_FT', 'H_ORB', 'H_DRB', 'H_AST', 'H_STL', 'H_BLK']
+
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'A_ORB', 'A_DRB', 'A_STL', 'A_BLK',
+    #             'H_FG', 'H_3P', 'H_FT', 'H_ORB', 'H_DRB', 'H_STL', 'H_BLK']
+
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'A_ORB', 'A_DRB',
+    #             'H_FG', 'H_3P', 'H_FT', 'H_ORB', 'H_DRB']
+
+    # 256: 59%
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'A_ORB',
+    #             'H_FG', 'H_3P', 'H_FT', 'H_ORB']
+
+    # 512: 59.8%
+    # use_cols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT',
+    #             'H_FG', 'H_3P', 'H_FT']
 
     data = np.array(pd.read_csv(file, header=0, usecols=use_cols)).tolist()
     data.sort(key=lambda x: x[0])
@@ -17,7 +40,7 @@ def features(file):
 
 
 def labels(file):
-    use_cols = ['GAME_ID', 'WINNER']
+    use_cols = ['GAME_ID', 'AWAY_POINTS', 'HOME_POINTS']
 
     data = np.array(pd.read_csv(file, header=0, usecols=use_cols)).tolist()
     data.sort(key=lambda x: x[0])
@@ -37,60 +60,47 @@ print(len(train_x), len(train_y))
 print(len(test_x), len(test_y))
 print(len(val_x), len(val_y))
 
-clf = RandomForestClassifier(n_estimators=500)
-clf.fit(train_x, train_y)
-preds = clf.predict(test_x)
+away_wins = 0
+home_wins = 0
+for game in train_y:
+    if game[0] > game[1]:
+        away_wins += 1
+    else:
+        home_wins += 1
+
+print(away_wins, home_wins)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=train_x[0].shape),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(2)
+])
+
+optimizer = tf.keras.optimizers.RMSprop(0.0001)
+model.compile(loss='mean_squared_error',
+              optimizer=optimizer,
+              metrics=['mean_squared_error', 'mean_absolute_error'])
+
+model.summary()
+
+early_stop = tf.keras.callbacks.EarlyStopping(min_delta=0.01, patience=10, restore_best_weights=True)
+csv_log = tf.keras.callbacks.CSVLogger('result_tracking/Feed Forward/No Rebounds.csv')
+
+history = model.fit(x=train_x, y=train_y, epochs=10000, callbacks=[early_stop, csv_log], validation_data=(test_x, test_y), shuffle=True)
+
+predictions = model.predict(val_x)
 
 wins = 0
-for pred, outcome in zip(preds, test_y):
-    if pred == outcome[0]:
+for pred, actual in zip(predictions, val_y):
+    if pred[0] > pred[1] and actual[0] > actual[1]:
         wins += 1
-
-print(wins, len(preds), float(wins / len(preds)))
-
-preds = clf.predict(val_x)
-wins = 0
-for pred, outcome in zip(preds, val_y):
-    if pred == outcome[0]:
+    elif pred[1] > pred[0] and actual[1] > actual[0]:
         wins += 1
+    # print(pred, actual)
 
-print(wins, len(preds), float(wins / len(preds)))
+print(wins, len(predictions), float(wins / len(predictions)))
 
-# away_wins = 0
-# home_wins = 0
-# for game in train_y:
-#     if game == 0:
-#         away_wins += 1
-#     else:
-#         home_wins += 1
-#
-# print(away_wins, home_wins)
-#
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(input_shape=train_x[0].shape),
-#     tf.keras.layers.Dense(512, activation=tf.nn.relu),
-#     tf.keras.layers.Dropout(0.1),
-#     tf.keras.layers.Dense(1, activation=tf.nn.softmax)
-# ])
-#
-# # optimizer = tf.keras.optimizers.RMSprop(0.001)
-# model.compile(loss='binary_crossentropy',
-#               optimizer='adam',
-#               metrics=['accuracy'])
-#
-# model.summary()
-#
-# early_stop = tf.keras.callbacks.EarlyStopping(min_delta=0.000001, patience=10, restore_best_weights=True)
-# csv_log = tf.keras.callbacks.CSVLogger('result_tracking/Feed Forward/No Rebounds.csv')
-#
-# model.fit(x=train_x, y=train_y, epochs=10000, callbacks=[early_stop, csv_log], validation_data=(test_x, test_y), shuffle=True)
-#
-# predictions = model.predict(val_x)
-#
-# wins = 0
-# for pred, actual in zip(predictions, val_y):
-#     if pred == actual:
-#         wins += 1
-#     # print(pred, actual)
-#
-# print(wins, len(predictions), float(wins / len(predictions)))
+plt.plot(history.history['val_loss'])
+plt.plot(history.history['loss'])
+plt.show()
--- select_features.py
+++ select_features.py
@@ -1,22 +1,53 @@
-from data import game_avg_features, game_avg_labels
 from feature_selector import FeatureSelector
 import pandas as pd
+import numpy as np
 
-# Todo load all features instead of just offensive stats
-features = pd.DataFrame(data=game_avg_features())
-labels = pd.DataFrame(data=game_avg_labels())
-labels = labels.drop(labels.columns[0], axis=1)  # Can only predict one value at a time
+
+def features(file='data/game_features_2016-19.csv', usecols=None):
+    if usecols is None:
+        usecols = ['GAME_ID', 'A_FG', 'A_3P', 'A_FT', 'H_FG', 'H_3P', 'H_FT']
+    else:
+        usecols = ['GAME_ID'] + usecols
+
+    data = np.array(pd.read_csv(file, header=0, usecols=usecols)).tolist()
+    data.sort(key=lambda x: x[0])
+    data = list(map(lambda x: x[1:], data))
+
+    return np.array(data)
+
+
+def labels(file='data/game_features_2016-19.csv'):
+    use_cols = ['GAME_ID', 'AWAY_POINTS', 'HOME_POINTS']
+
+    data = np.array(pd.read_csv(file, header=0, usecols=use_cols)).tolist()
+    data.sort(key=lambda x: x[0])
+    data = list(map(lambda x: x[1:], data))
+
+    return np.array(data)
+
 
 # Set features header
 columns = ['A_FG', 'A_FGA', 'A_3P', 'A_3PA', 'A_FT', 'A_FTA', 'A_ORB', 'A_DRB', 'A_TRB', 'A_AST', 'A_STL', 'A_BLK', 'A_TOB', 'A_PF', 'A_AVG_PTS',
                 'H_FG', 'H_FGA', 'H_3P', 'H_3PA', 'H_FT', 'H_FTA', 'H_ORB', 'H_DRB', 'H_TRB', 'H_AST', 'H_STL', 'H_BLK', 'H_TOB', 'H_PF', 'H_AVG_PTS']
 
+# Todo load all features instead of just offensive stats
+features = pd.DataFrame(data=features(usecols=columns))
+labels = pd.DataFrame(data=labels())
+# labels = labels.drop(labels.columns[0], axis=1)  # Can only predict one value at a time
+
 features.columns = columns
+print(len(features.columns), len(columns))
 
 fs = FeatureSelector(data=features, labels=labels)
 fs.identify_missing(missing_threshold=0.9)
-fs.identify_collinear(correlation_threshold=0.6)
-# fs.identify_zero_importance(eval_metric='l2', task='regression')
+fs.identify_collinear(correlation_threshold=0.5)
+fs.plot_collinear()
+fs.identify_zero_importance(eval_metric='l2', task='regression')
+
+# Drop:
+# FGA, 3PA, FTA
+# TRB
+# AVG_PTS
 
 print(fs.record_collinear.head())
 
